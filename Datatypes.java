5c44dd6e24465829aa898c66ed88ed62c3cef772
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-11 20:30:15 +0530

    start the java DSA journey

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+public class Datatypes {
+  public static void main(String[] args) {
+    int a = 20 ;
+        System.out.println(a);
+        short num2 = 4000;
+        System.out.println(num2);
+        float f = 8.0f;
+        System.out.println(f);
+        double d = 85696.2650055;
+        System.out.println(d);
+        long l = 20596633;
+        System.out.println(l);
+        char c = 'a';
+        byte b= 10;
+        System.out.println(b);
+        System.out.println("char ASCII is " + (char) + (c+10));
+  }
+  
+}
---
+++ ImplicitConv.java
@@ -0,0 +1,7 @@
+public class ImplicitConv {
+  public static void main(String[] args) {
+    int num = 20 ;
+    long num2 = num ;
+    System.out.println("convert for one datatype to other:" + num2 );
+  }
+}
