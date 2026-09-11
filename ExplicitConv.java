3ccc03b31af905b966dcb490f7c299e145712bf0
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-11 20:32:45 +0530

    Expicit program

---
+++ ExplicitConv.java
@@ -0,0 +1,8 @@
+public class ExplicitConv {
+  public static void main(String[] args) {
+    long l = 565546498;
+    int exp = (int)l;
+    System.out.println("you data is converted explicitily : " + exp);
+
+  }
+}
