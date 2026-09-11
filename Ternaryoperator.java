7e62d8c71421b703260ee8c5c5a917bebd0c2465
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-12 00:14:51 +0530

    ternary operator

---
+++ Ternaryoperator.java
@@ -0,0 +1,7 @@
+public class Ternaryoperator {
+  public static void main(String[] args) {
+    int streakdays = 35;
+    String status =(streakdays >=30 )?"consistent" : "irregular";
+    System.out.println(status);
+  }
+}
