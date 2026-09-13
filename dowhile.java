497502dae7f08ce979aea828a83b370c6ed229fe
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-13 07:09:15 +0530

    dowhile loop

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+  public static void main(String[] args) {
+    int n=1;
+    do { 
+        if(n%2!=0){
+          System.out.println(n);
+        }
+          n++;
+        
+    } while (n<=20);
+  }
+  
+}
---
+++ nestedwhile.java
@@ -0,0 +1,14 @@
+public class nestedwhile{
+  public static void main(String[] args) {
+    int i =1;
+    while(i<=4){
+      int j=1;
+      while(j<=4){
+        System.out.print(i+ "" +j +"" );
+        j++;
+      }
+      System.out.println(" ");
+      i++;
+    }
+  }
+}
