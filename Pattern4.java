606f73d31dcfb0152f9a5175feb585880882229a
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-13 18:06:16 +0530

    pattern4

---
+++ Pattern4.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Pattern4{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int n = sc.nextInt();
+    for(int row=1; row<=n; row++){
+      for(int col=1; col<=n-row+1; col++){
+      System.out.print("*");
+    }
+    System.out.println(" ");
+
+  }
+}
+}
