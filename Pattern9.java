e0d41dd4ab67722423ed6d95c58e9a0c8ff9cec6
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-15 23:24:56 +0530

    git update

--- Pattern9.java
+++ Pattern9.java
@@ -1,23 +1,28 @@
-public class Pattern9{
-  public static void main(String[] args) {
-    int n=5;
-    for(int row =1; row<=n; row++){
-      //for each row  -> variable columns
-      if(row == 1 || row == 2 || row==n){
-        for(int col=1;col<=row;col++){
-          System.out.print("* ");
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+
+            if (row == 1 || row == 2 || row == n) {
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+                }
+            } else {
+                // First *
+                System.out.print("* ");
+
+                // Spaces between the stars
+                for (int col = 1; col <= row - 2; col++) {
+                    System.out.print("  ");
+                }
+
+                // Last *
+                System.out.print("* ");
+            }
+
+            // Move to next row
+            System.out.println();
         }
-      }
-      else{
-        //middle rows
-        //1*
-        System.out.println("* ");
-      }
-      for(int col=1; col<=(row-2); col++){
-        System.out.println(" ");
-      }
-      System.out.println("* ");
-      //move to next row
-      System.out.println();
-  }
-  
+    }
+}
