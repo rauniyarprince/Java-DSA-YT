97b8a9a0ce37064b0a0abfa6d6f84df3ab15bd7b
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-18 22:27:06 +0530

    update

---
+++ pattern12.java
@@ -0,0 +1,37 @@
+public class pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper half
+        for (int row = 1; row <= n; row++) {
+
+            // spaces
+            for (int col = 1; col <= 2 * (n - row); col++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int col = 1; col <= 2 * row - 1; col++)
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int row = 2; row <= n; row++) {
+
+            // spaces
+            for (int col = 1; col <= 2 * (row - 1); col++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int col = 1; col <= 2 * n - 2 * row + 1; col++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
