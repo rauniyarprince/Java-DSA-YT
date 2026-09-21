dba4ab7a552f0aae3457db2d901c746dd13b189b
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-21 23:42:51 +0530

    upadte the error

--- prog.java
+++ prog.java
@@ -1,4 +1,8 @@
 public class prog{
   public static void main(String[] args) {
     int n= 20, b=20, c=34,d=45;
-    float avg = (n+b
+    float avg = (n+b+c+d/4);
+    System.out.println(avg);
+    
+  }
+}
