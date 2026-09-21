7b02086e061076bcbcd17c0ef604bc12ea2d2fcd
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-21 23:42:32 +0530

    udpate

--- prog.java
+++ prog.java
@@ -1,8 +1,4 @@
 public class prog{
   public static void main(String[] args) {
     int n= 20, b=20, c=34,d=45;
-    float avg = (n+b+c+d/4);
-    System.out.println(avg);
-    
-  }
-}
+    float avg = (n+b
